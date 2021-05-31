c7715e961c76a3f6398902a574402bfe6773f54b
李昆明(likunming1) <likunming1@longfor.com>
2021-05-31 18:02:31 +0800

    CSS三等分尝试

--- 代码/03-JavaScript基础/04-Vue使用/todo-vue/src/App.vue
+++ 代码/03-JavaScript基础/04-Vue使用/todo-vue/src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="app">
-      <h1>My ToTo List</h1>
+      <!-- <h1>My ToTo List</h1>
       <to-do-form></to-do-form>
       <ul>
         <li v-for="item in ToDoItems" :key="item.id">
           <to-do-item :label="item.label" :done="item.done"> </to-do-item>
         </li>
-      </ul>
- 
-  </div>
+      </ul> -->
+      <div class="blank">
+
+      </div>
+      <div id="card">
+        <label id="cardTitle" for="智慧服务"></label>
+        <img id="ball" src="./assets/logo.png" alt="Vue logo">
+
+        <div class="card_header_container">
+          <div class="header_title">重大事故1</div>
+          <div class="header_title">重大事故2</div>
+          <div class="header_title">重大事故3</div>
+        </div>
+      </div>
+
+    </div>
+  
 </template>
 
 <script>
@@ -36,47 +50,61 @@ export default {
 </script>
 
 <style>
-/* #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-} */
-/* Global styles */
-
-/* End global styles */
-#app {
-  background: #fff;
+}
+#blank {
+  height: 50px;
+  width: 100%;
+  background: rebeccapurple;
+}
+#card {
+  background: blue;
+  height: 20rem;
   margin: 2rem 0 4rem 0;
   padding: 1rem;
-  padding-top: 0;
+  padding-top: 50;
   position: relative;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
 }
-@media screen and (min-width: 550px) {
-  #app {
-    padding: 4rem;
-  }
+#ball {
+  top: 15px;
+  height: 50px;
+  width: 50px;
+  
 }
-#app > * {
-  max-width: 50rem;
-  margin-left: auto;
-  margin-right: auto;
+#cardTitle {
+  top: 15px;
+  background: orange;
+  height: 21px;
+  width: 100px;
+  float: left;
 }
-
-#app > form {
+#card_header_container {
+  height: 20px;
   max-width: 100%;
+  display: flex;
+}
+#header_title {
+  background: red;
+  flex: 1;
+  height: 20px;
 }
 
-#app h1 {
-  display: block;
-  min-width: 100%;
-  width: 100%;
-  text-align: center;
-  margin: 0;
-  margin-bottom: 1rem;
+.card_header_container .header_title:nth-of-type(1) {
+  padding-right: 10px;
+}
+.card_header_container .header_title:nth-of-type(2) {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.card_header_container .header_title:nth-of-type(3) {
+  padding-left: 10px;
 }
 
 </style>
