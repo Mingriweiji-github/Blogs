e60cf665786200c2bc43af8d789dce059cf5e9c3
李昆明(likunming1) <likunming1@longfor.com>
2021-05-31 16:00:03 +0800

    CSS 卡片背景

--- 代码/03-JavaScript基础/04-Vue使用/todo-vue/src/App.vue
+++ 代码/03-JavaScript基础/04-Vue使用/todo-vue/src/App.vue
@@ -36,12 +36,47 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+/* Global styles */
+
+/* End global styles */
+#app {
+  background: #fff;
+  margin: 2rem 0 4rem 0;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
 }
+@media screen and (min-width: 550px) {
+  #app {
+    padding: 4rem;
+  }
+}
+#app > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#app > form {
+  max-width: 100%;
+}
+
+#app h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
+}
+
 </style>
