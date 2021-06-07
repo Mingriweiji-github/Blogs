5d978d00b1f28a204938e5aef389bc881faa6ab0
李昆明(likunming1) <likunming1@longfor.com>
2021-06-07 11:59:23 +0800

    vue卡片列表

---
+++ 代码/04-vue卡片和列表/src/components/ToDoForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <form>
+        <label for="new-todo-input">
+            What needs to be done?
+        </label>
+        <input 
+            type="text"
+            id="new-todo-input"
+            name="new-todo"
+            autocomplete="off"
+        />
+
+        <button type="submit">
+            Add
+        </button>
+    </form>
+</template>
+
+<script>
+export default { };
+</script>
---
+++ 代码/04-vue卡片和列表/src/components/ToDoItem.vue
@@ -0,0 +1,25 @@
+
+
+<template>
+   <div>
+        <input type="checkbox" :id="id" :checked="isDone" />
+        <label :for="id"> {{label}}</label>
+    </div>
+</template>
+
+<script>
+    import uniqueId from "lodash.uniqueid";
+
+    export default {
+        props: {
+            label: {required: true, type: String},
+            done: {default: false, type: Boolean}
+        },
+        data() {
+            return {
+                isDone: this.done,
+                id: uniqueId('todo-')
+            };
+        }
+    }
+</script>
