c598acea511c3385949cd1ba543a0eb95b47483f
李昆明(likunming1) <likunming1@longfor.com>
2021-06-21 16:01:10 +0800

    flex布局

---
+++ 代码/07-vue-flexbox1/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <div class="main">
+      <div class="flex1">Flex布局1</div>
+      <div class="flex2">Flex布局2</div>
+      <div class="flex4">插入Flex布局4插入Flex布局4插入Flex布局4插入Flex布局4</div>
+      <div class="flex3">Flex布局第3</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+  /* background-color: purple; */
+}
+.main {
+  background-color: wheat;
+  width: 100%;
+  height: 200px;
+  display: flex;
+  /* flex-direction: row;
+  flex-wrap: wrap; */
+  flex-flow: row wrap;
+  align-content: space-between;
+}
+
+
+.flex1 {
+  background-color: red;    
+}
+
+  .flex2 {
+    background-color: green;
+  }
+  .flex3 {
+    background-color: blue;
+  }
+  
+/* .space-around {
+  justify-content: space-around;
+}
+
+.center {
+  justify-content: center;
+}
+
+.end {
+  justify-content: flex-end;
+} */
+
+
+
+
+</style>
---
+++ 代码/07-vue-flexbox2/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="main">
+      <div class="flex1">前端小课flex item属性</div>
+      <div class="flex2">FlexBox World</div>
+      <div class="flex3">Flex布局33</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+  /* background-color: purple; */
+}
+.main {
+  background-color: wheat;
+  width: 600px;
+  height: 200px;
+  display: flex;
+}
+
+
+div div {
+  height: 40px;
+  font-size: 28px;
+  text-align: center;
+  flex-basis: 0;
+  flex-grow: 1;
+}
+.flex1 {
+  background-color: red;
+  
+}
+
+.flex2 {
+  background-color: green;
+}
+.flex3 {
+  background-color: blue;
+}
+
+
+
+
+
+
+</style>
